a6bf109852d80db15e240bad1408e290b3340ab5
azd <azd171819@163.com>
2021-08-21 17:18:03 +0800

    commit"2021.8.21-2"

---
+++ yygh_parent/service/service_user/src/main/java/com/atguigu/yygh/user/controller/WeixinApiController.java
@@ -0,0 +1,41 @@
+package com.atguigu.yygh.user.controller;
+
+import com.atguigu.yygh.common.result.Result;
+import com.atguigu.yygh.user.config.ConstantPropertiesUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/api/ucenter/wx")
+public class WeixinApiController {
+
+//    @Autowired
+//    private UserInfoService userInfoService;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 获取微信登录参数
+     */
+    @GetMapping("getLoginParam")
+    @ResponseBody
+    public Result genQrConnect(HttpSession session) throws UnsupportedEncodingException {
+        String redirectUri = URLEncoder.encode(ConstantPropertiesUtil.WX_OPEN_REDIRECT_URL, "UTF-8");
+        Map<String, Object> map = new HashMap<>();
+        map.put("appid", ConstantPropertiesUtil.WX_OPEN_APP_ID);
+        map.put("redirectUri", redirectUri);
+        map.put("scope", "snsapi_login");
+        map.put("state", System.currentTimeMillis()+"");//System.currentTimeMillis()+""
+        return Result.ok(map);
+    }
+}
